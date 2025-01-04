445086b7349a98e1a7c9cd832c67ad62b7a68292
Danilo Miranda <nilomiranda3@gmail.com>
2025-01-03 19:29:27 -0800

    refactoring to use standard http libs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,20 +4,16 @@ import (
 	"fmt"
 	"net/http"
 	"nilomiranda/web-alerts/internal/handlers"
-
-	"github.com/go-chi/chi"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
   log.SetReportCaller(true)
-  var router *chi.Mux = chi.NewRouter()
-
-  handlers.Handler(router)
-
   fmt.Println("Starting server.")
 
-  err := http.ListenAndServe("localhost:8000", router)
+  handlers.InitiateRouter()
+
+  err := http.ListenAndServe(":8000", nil)
 
   if err != nil {
     log.Error(err)
--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -1,17 +1,16 @@
 package handlers
 
 import (
+	"fmt"
+	"net/http"
 	"nilomiranda/web-alerts/internal/routes"
-
-	"github.com/go-chi/chi"
-	// middleware "github.com/go-chi/chi/middleware"
-	// "danilo/web-alerts/internal/middlware"
 )
 
-func Handler(r *chi.Mux) {
-  // r.Use(middleware.StripSlashes)
-  
-  r.Route("/api", func(r chi.Router) {
-    r.Get("/health", routes.GetHealth)
-  })
+type HTTPHandler struct {}
+func (httpHandler *HTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  fmt.Println("Handling incoming requests...")
+}
+
+func InitiateRouter() {
+  http.HandleFunc("/api/health", routes.GetHealth)
 }
